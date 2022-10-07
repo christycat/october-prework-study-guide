e4063e382d48cf2eb773baf49c039cd727377d41
christycat <christymorton77@gmail.com>
2022-10-07 11:39:22 -0400

    added js code

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+for(var x = 0; x < topics.length; x++) {
+ console.log(topics[x]);
 }
+
+
 /*  
+var topic = "HTML";
+
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -14,5 +17,5 @@ if (topic === 'HTML') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
-}
-*/  
+} 
+*/
